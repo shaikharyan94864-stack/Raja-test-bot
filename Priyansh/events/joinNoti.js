600d6a7843e9c6109beb5b3d3f84315d1a4d4ad7
shaikharyan94864-stack <shaikharyan94864@gmail.com>
2025-09-08 22:53:54 +0530

    Update joinNoti.js

--- Priyansh/events/joinNoti.js
+++ Priyansh/events/joinNoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "joinNoti",
     eventType: ["log:subscribe"],
     version: "1.0.1",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "𝑺𝒂𝒉𝒊𝒃𝒂 𝒔𝒌",
     description: "Notification of bots or people entering groups with random gif/photo/video",
     dependencies: {
         "fs-extra": "",
